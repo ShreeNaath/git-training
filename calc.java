6c98a64ae36f5fa4c5f7b42c4ed7ec2b6805184a
Shree Naath <shreenaath.sridhar@a-cti.com>
2016-04-09 13:08:25 +0530

    Rectified errors - Project is in Working state with add,sub

--- calc.java
+++ calc.java
@@ -1,23 +1,32 @@
 import java.util.Scanner;
-public class calc extends add
+public class calc extends Calc_func
 {
+    static Scanner s=new Scanner(System.in);
+    static int a,b;
+    public static void get()
+    {
+        System.out.println("Enter the two numbers :");
+        a=s.nextInt();
+        b=s.nextInt();
+    }
     public static void main(String[] args)
     {
         Scanner s=new Scanner(System.in);
-        int option,a,b;
+        int option;
         do
         {
             System.out.println("Menu:\n1.Add\t2.Sub\t3.Exit");
             System.out.println("Enter the option :");
             option=s.nextInt();
-            System.out.println("Enter the two numbers :");
-            a=s.nextInt();
-            b=s.nextInt();
+            
             switch(option)
             {
-                case 1:Calc_func.add(a,b);
+                case 1:
+                    get();
+                    Calc_func.add(a,b);
                     break;
                 case 2:
+                    get();
                     Calc_func.sub(a,b);
                     break;
                 case 3:return;
